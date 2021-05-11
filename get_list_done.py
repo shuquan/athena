c124ee43e4478e12d09591d473f5c36f6a32721b
Shuquan Huang <shuquan@Shuquans-MacBook-Pro-2019.local>
2021-05-11 23:26:56 +0800

    Locate to the List Done Page

--- get_list_done.py
+++ get_list_done.py
@@ -13,5 +13,9 @@ login_password.send_keys('')
 login_button = driver.find_element_by_id('login_button')
 login_button.submit()
 
+js="document.getElementsByClassName('lev2 lev')[1].style.display='block'"
+driver.execute_script(js)
+driver.find_element_by_id('F01_listDone').click()
+
 time.sleep(5) # Let the user actually see something!
 driver.quit()
