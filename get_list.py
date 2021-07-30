3d2fa3f26979d1127f42a3d18a9f0ca17a19edb9
Shuquan Huang <shuquan.huang@live.com>
2021-07-30 15:48:45 +0800

    Get contract detail

--- get_list.py
+++ get_list.py
@@ -6,6 +6,8 @@ from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
 
 from athena.objects import objects
 
@@ -64,6 +66,56 @@ def procurement_handler(total_procurement, record, driver):
 
 def contracts_handler(total_contracts, record, driver):
     logging.info('[合同]%s' % driver.title)
+    iframe = driver.find_element_by_id('zwIframe')
+    driver.switch_to.frame(iframe)
+    while driver.execute_script("return document.readyState") != "complete" :
+        time.sleep(1)
+        logging.info('[合同]%s：等待iframe加载' % driver.title)
+    rows = driver.find_elements_by_css_selector('section [class*="browse"]')
+    while len(rows) != 52:
+        time.sleep(1)
+        logging.info('[合同]%s：需要加载52条，已经加载%s条，等待' % (driver.title,len(rows)))
+        rows = driver.find_elements_by_css_selector('section [class*="browse"]')
+    logging.info('[合同]%s：需要加载52条，已经加载%s条' % (driver.title,len(rows)))
+    record['合同编号'] = rows[2].get_attribute('textContent')
+    record['申请人'] = rows[3].get_attribute('textContent')
+    record['申请部门'] = rows[4].get_attribute('textContent')
+    record['申请日期'] = rows[5].get_attribute('textContent')
+    record['客户单位名称'] = rows[6].get_attribute('textContent')
+    record['履行地点'] = rows[7].get_attribute('textContent')
+    record['区域'] = rows[10].get_attribute('textContent')
+    record['合同名称'] = rows[16].get_attribute('textContent')
+    record['合同价'] = rows[17].get_attribute('textContent')
+    record['硬件金额'] = rows[19].get_attribute('textContent')
+    record['成本'] = rows[20].get_attribute('textContent')
+    record['合同内容'] = rows[21].get_attribute('textContent')
+    record['服务期限'] = [rows[22].get_attribute('textContent'),rows[23].get_attribute('textContent')]
+    record['收款方式'] = rows[24].get_attribute('textContent')
+    record['备注'] = rows[25].get_attribute('textContent')
+    record['项目编号'] = rows[27].get_attribute('textContent')
+    record['项目名称'] = rows[28].get_attribute('textContent')
+    record['软件许可数'] = rows[29].get_attribute('textContent')
+    record['软件许可数'] = rows[29].get_attribute('textContent')
+    record['纯服务合同'] = rows[30].get_attribute('textContent')
+    record['运维服务'] = rows[31].get_attribute('textContent')
+    record['产品'] = rows[32].get_attribute('textContent')
+    record['产品分类'] = [rows[33].get_attribute('textContent'), rows[34].get_attribute('textContent')]
+    record['业务来源'] = rows[35].get_attribute('textContent')
+    record['业务需求'] = rows[36].get_attribute('textContent')
+    record['行业类型'] = rows[37].get_attribute('textContent')
+    record['所属大区'] = rows[38].get_attribute('textContent')
+    record['签署收款金额'] = rows[39].get_attribute('textContent')
+    record['到货收款金额'] = rows[40].get_attribute('textContent')
+    record['初验收款金额'] = rows[41].get_attribute('textContent')
+    record['终验收款金额'] = rows[42].get_attribute('textContent')
+    record['保内尾款金额'] = rows[43].get_attribute('textContent')
+    record['其他'] = rows[44].get_attribute('textContent')
+    record['合同约定付款节点'] = rows[45].get_attribute('textContent')
+    record['收款节点总计'] = rows[46].get_attribute('textContent')
+    record['比例合计'] = rows[47].get_attribute('textContent')
+    record['盖章人'] = rows[49].get_attribute('textContent')
+    record['盖章状态'] = rows[50].get_attribute('textContent')
+    record['归档确认'] = rows[51].get_attribute('textContent')
     total_contracts.append(record)
 
 def weekly_reports_handler(total_weekly_reports, record, driver):
@@ -72,6 +124,9 @@ def weekly_reports_handler(total_weekly_reports, record, driver):
 
     iframe = driver.find_element_by_id('zwIframe')
     driver.switch_to.frame(iframe)
+    while driver.execute_script("return document.readyState") != "complete" :
+        time.sleep(1)
+        logging.info('[周报]%s：等待iframe加载' % driver.title)
     rows = driver.find_elements_by_css_selector('.is-detailshover')
     for row in rows:
         tds = row.find_elements_by_css_selector('section [class*="browse"]')
@@ -197,14 +252,21 @@ def main():
             record = {'标题': subject, '发起人':start_member_name, '上一处理人':pre_approver_name, '发起时间':start_date, '处理时间':deal_time, '当前代办人':current_nodes_info}
             total_records.append(record)
 
+            # Check the message setting and ignore it
+            msgsettingmenu = driver.find_elements_by_class_name('msgSettingMenu')
+            if len(msgsettingmenu) > 1:
+                msgsettingmenu[1].click()
             # Click each row and Open new tab to fetch more data
-            webdriver.ActionChains(driver).move_to_element(row).click(row).perform()
+            try:
+                webdriver.ActionChains(driver).move_to_element(row).click(row).perform()
+            except StaleElementReferenceException:
+                webdriver.ActionChains(driver).move_to_element(row).click(row).perform()
             WebDriverWait(driver, 5).until(EC.number_of_windows_to_be(2))
             window_handles = driver.window_handles
             driver.switch_to.window(window_handles[1])
             if '出差单' in subject:
                 travels_handler(total_travels, record, driver)
-            elif '合同' in subject:
+            elif '销售合同' in subject:
                 contracts_handler(total_contracts, record, driver)
             elif '周报' in subject:
                 weekly_reports_handler(total_weekly_reports, record, driver)
@@ -223,7 +285,8 @@ def main():
             break
 
         page_next_button = driver.find_element_by_class_name('pageNext')
-        page_next_button.click()
+        webdriver.ActionChains(driver).move_to_element(page_next_button).click(page_next_button).perform()
+        #page_next_button.click()
         time.sleep(1)
 
     driver.quit()
